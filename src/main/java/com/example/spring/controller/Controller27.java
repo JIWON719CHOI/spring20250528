5793fcac6e509ee3d72a744c9747793d36393634
JIWON CHOI <jiwon719choi@gmail.com>
2025-06-19 17:28:30 +0900

    DB 복사하기.. 원래 DB는 클라우드 저장소같은 별도의 백업장치를 사용하는 것이 좋다.

--- src/main/java/com/example/spring/controller/Controller27.java
+++ src/main/java/com/example/spring/controller/Controller27.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -24,6 +25,23 @@ public class Controller27 {
     @PostMapping("sub1")
     public String post1(String username, MultipartFile upload) {
         service9.action1(username, upload);
+
+        return "redirect:/main27/sub1";
+    }
+
+    @PostMapping("sub2")
+    public String post2(String address,
+                        @RequestParam(name = "uploadFile", required = false)
+                        MultipartFile upload) {
+
+        service9.action2(address, upload);
+
+        return "redirect:/main27/sub1";
+    }
+
+    @PostMapping("sub3")
+    public String post3(MultipartFile[] upload) {
+        service9.action3(upload);
 
         return "redirect:/main27/sub1";
     }
--- src/main/java/com/example/spring/service/Service9.java
+++ src/main/java/com/example/spring/service/Service9.java
@@ -37,4 +37,61 @@ public class Service9 {
             new RuntimeException(e);
         }
     }
+
+    public void action2(String address, MultipartFile upload) {
+        System.out.println("address = " + address);
+        System.out.println("upload = " + upload.getOriginalFilename());
+
+        if (upload != null) {
+            if (upload.getSize() > 0) {
+                try {
+                    // inputStream
+                    // outputStream
+                    String path = "C:/Temp/uploadFiles/" + upload.getOriginalFilename();
+                    OutputStream os = new FileOutputStream(path);
+                    BufferedOutputStream bos = new BufferedOutputStream(os);
+
+                    InputStream is = new BufferedInputStream(upload.getInputStream());
+
+                    try (is; bos; os;) {
+//                        is.transferTo(bos);
+
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = is.read(buffer)) != -1) {
+                            bos.write(buffer, 0, length);
+                        }
+                        bos.flush();
+                    }
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void action3(MultipartFile[] upload) {
+        if (upload != null) {
+            for (MultipartFile file : upload) {
+                if (file.getSize() > 0) {
+                    try {
+                        String path = "C:/Temp/uploadFiles/" + file.getOriginalFilename();
+                        var bis = new BufferedInputStream(file.getInputStream());
+                        var bos = new BufferedOutputStream(new FileOutputStream(path));
+                        try (bos; bis) {
+                            byte[] buffer = new byte[1024];
+                            int length;
+                            while ((length = bis.read(buffer)) != -1) {
+                                bos.write(buffer, 0, length);
+                            }
+                            bos.flush();
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
 }
